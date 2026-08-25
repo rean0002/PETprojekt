1987a27216f47e722a25844e598d7705e35ff3e6
rean0002 <rean0002@stud.kea.dk>
2026-08-25 15:06:56 +0200

    user klasse

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -7,26 +7,29 @@ public class User {
     private String password;
     private String name;
     private int point;
+    private Role role;
 
-    public User(int id, String email, String password, String name, int point) {
+    public User(int id, String email, String password, String name, int point, Role role) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.name = name;
         this.point = point;
+        this.role = role;
     }
 
-    public Task createTask(String title, String description, Category category,
-                           LocalDate date, LocalTime time, Frequency frequency, boolean reassignable) {
-        return new Task(title, description, category, date, time, frequency, this, reassignable);
+    public Task createTask(String title, String description, TaskCategory category,
+                           LocalDate date, LocalTime time, TaskFrequency frequency, boolean reassignable) {
+        return new Task(title, description, category, date, time, frequency, reassignable);
     }
+
     public void taskDone(Task task) {
         task.markAsDone();
         this.point += 1;
     }
 
     public Reminder createReminder(String description, LocalDate date, LocalTime time) {
-        return new Reminder(description, date, time, this);
+        return new Reminder(description, date, time);
     }
     public int getId() {
         return id;

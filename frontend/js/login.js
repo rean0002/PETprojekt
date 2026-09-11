1168a961607770aba9cf24181f3137a241c9d031
rean0002 <rean0002@stud.kea.dk>
2026-09-11 12:22:32 +0200

    styling af login.html og opret-opgave.html

---
+++ frontend/js/login.js
@@ -0,0 +1,5 @@
+document.getElementById('login-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // evt. validering/login-logik her
+    window.location.href = 'dashboard.html';
+});

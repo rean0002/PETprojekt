5c6c2a3ae3865934f6867884d6d1a1296e17a8ea
rean0002 <rean0002@stud.kea.dk>
2026-09-18 09:26:14 +0200

    styling af login.html og opret-opgave.html

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -2,22 +2,31 @@ package entities;
 
 public class User {
     private int id;
-    private String username; //skal slettes
     private String email;
     private String password;
-    private String name;
     private int point;
     private Role role;
+    private String firstName;
+    private String lastName;
 
     // rigtige konstruktør
-    public User(int id, String email, String password, String name, int point, Role role) {
+    public User(int id, String email, String password, int point, Role role, String firstName, String lastName) {
         this.id = id;
         this.email = email;
         this.password = password;
-        this.name = name;
         this.point = point;
         this.role = role;
+        this.firstName= firstName;
+        this.lastName= lastName;
     }
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName=lastName;
+
+    }
+
 
     // opgave kostruktør
     public User(String email, String password) {
@@ -26,11 +35,12 @@ public class User {
     }
 
     public int getId() { return id; }
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
     public String getEmail() { return email; }
     public String getPassword() { return password; }
-    public String getName() { return name; }
+
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+
     public int getPoint() { return point; }
     public Role getRole() { return role; }
 }

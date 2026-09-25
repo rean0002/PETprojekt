a580e22485a3a01fe6e2b2277c26a0e97a126bf0
Olga-Willadsen <olwi1000@stud.ek.dk>
2026-09-25 11:38:21 +0200

    random kode

--- src/main/java/entities/Household.java
+++ src/main/java/entities/Household.java
@@ -28,14 +28,19 @@ public class Household {
     }
 
     public String code(){
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         String [] code = new String[6];
         Random r= new Random();
 
-        code[0]=
+        code[0]= String.valueOf(alphabet.charAt(r.nextInt(alphabet.length())));
+        code[1]= String.valueOf(alphabet.charAt(r.nextInt(alphabet.length())));
+        code[2]= String.valueOf(alphabet.charAt(r.nextInt(alphabet.length())));
+        code[3]= String.valueOf(alphabet.charAt(r.nextInt(alphabet.length())));
+        code[4]= String.valueOf(alphabet.charAt(r.nextInt(alphabet.length())));
+        code[5]= String.valueOf(alphabet.charAt(r.nextInt(alphabet.length())));
 
 
-
-        return code[0]+code[1]+code[2]+code[3]+code[4]+code[5];
+        return (code[0]+code[1]+code[2]+code[3]+code[4]+code[5]);
     }
 
 
